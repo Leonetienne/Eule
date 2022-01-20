131a2239b3c721f8f0030964f48e403c99709861
Leonetienne <leonetienne@hotmail.de>
2022-01-20 22:25:12 +0100

    Added include-directory for single header deployments

---
+++ INCLUDE/generate.sh
@@ -0,0 +1,8 @@
+# Build the include files
+echo "Building the include files..."
+deggl -i ../Eule/*.cpp -o Eule
+
+# Verify that they compile cleanly
+echo "Verifying that they compile"
+g++ Eule.cpp -c -S -o - -Wall -Wextra -Wpedantic > /dev/null
+
